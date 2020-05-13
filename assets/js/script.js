b9a9943eea80a289a89681b1ff27494de02ed861
htmlcssphpjs <63463168+htmlcssphpjs@users.noreply.github.com>
2020-05-13 16:28:52 +0300

    https://zen.yandex.ru/id/5e7c78ee99d560276a9df6e4

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//js//
+console.log("JS активен");
+console.log("https://zen.yandex.ru/id/5e7c78ee99d560276a9df6e4");
+
+//###//ФУНКЦИИ\\###\\
+function zaglushka () {
+    alert("!Пока не сделано!");
+    console.log("!Сработала заглушка!");
+}
+function entrens () {
+    alert("!!!В данный момент вход не доступен!!!");
+}
+
+//сюда залезезеет только Влад ;)
+console.log("Сюда залезезеет только Влад(или Артём)");
